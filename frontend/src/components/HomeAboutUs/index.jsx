48f9e22f2a35c6b0862f3baae6561c259c2ed154
Cristian Rubio <cristianfrubio01@gmail.com>
2024-10-18 13:02:06 -0500

    Se agrega componente de los premios que tiene la organizacion

--- frontend/src/components/HomeAboutUs/index.jsx
+++ frontend/src/components/HomeAboutUs/index.jsx
@@ -10,9 +10,8 @@ const HomeAboutUs = () => {
           <img src={image} alt="aboutUs" />
         </div>
         <div className="contHomeAboutUs">
-          <h3>¿QUIÉNES SOMOS?</h3>
-          <p>«CONASEGUR» Corporación Nacional de Empresas de Seguridad Privada, que propenden por el desarrollo gremial de sus asociados.</p>
-          <p>Somos pioneros en la búsqueda,  ...</p>
+          <h4>¿QUIÉNES SOMOS?</h4>
+          <p>«CONASEGUR» Corporación Nacional de Empresas de Seguridad Privada, que propenden por el desarrollo gremial de sus asociados...</p>
           <button>Conocer más</button>
         </div>
       </div>
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { HomeSlider } from '../../components/HomeSlider';
 import { HomeAboutUs } from '../../components/HomeAboutUs';
 import { HomeMAndV } from '../../components/HomeMAndV';
+import { HomeAwards } from '../../components/HomeAwards';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HomeSlider />
       <HomeAboutUs />
       <HomeMAndV />
+      <HomeAwards />
     </div>
   );
 };
