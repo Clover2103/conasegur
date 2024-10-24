313360231be8c58533b9eca730ed7a7dbacb2ffb
Cristian Rubio <cristianfrubio01@gmail.com>
2024-10-24 16:15:08 -0500

    Se incorpora componenete para formulario de contacto y se modifica el navbar para visualizar correctamente su sombra de caja

--- frontend/src/pages/Contacte/index.jsx
+++ frontend/src/pages/Contacte/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { ContactForm } from '../../components/ContactForm';
 
 const Contacte = () => {
   return (
     <div>
-      Contacte
+      <ContactForm />
     </div>
   );
 };
